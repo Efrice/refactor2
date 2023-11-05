da42d3344cd9d05f1f8bba02d7e2a08b6fb39810
efrice <719431749@qq.com>
2023-11-05 20:26:47 +0800

    chore: init repo

---
+++ src/test/start.spec.js
@@ -0,0 +1,17 @@
+import { expect, test } from 'vitest'
+import plays from '../data/plays.json'
+import invoices from '../data/invoices.json'
+import { statement } from '../start.js'
+
+test('statement', () => {
+  const result = statement(invoices[0], plays)
+  expect(result).toMatchInlineSnapshot(`
+    "Statement for BigCo
+     Hamlet: $650.00 (55 seats)
+     As You Like It: $580.00 (35 seats)
+     Othello: $500.00 (40 seats)
+    Amount owed is $1,730.00
+    You earned 47 credits
+    "
+  `)
+})
