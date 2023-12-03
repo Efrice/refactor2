d5680cfc0fcf52f0fd4e59e1fb151177115aa3a0
efrice <719431749@qq.com>
2023-12-03 17:49:52 +0800

    refactor: step14 remove variable volumeCredits

--- src/test/start.spec.js
+++ src/test/start.spec.js
@@ -14,7 +14,8 @@ import invoices from '../data/invoices.json'
 // import { statement } from '../step10.js'
 // import { statement } from '../step11.js'
 // import { statement } from '../step12.js'
-import { statement } from '../step13.js'
+// import { statement } from '../step13.js'
+import { statement } from '../step14.js'
 
 test('statement', () => {
   const result = statement(invoices[0], plays)
