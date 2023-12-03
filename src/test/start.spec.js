7277d94c649f7b51191ec9db72a074e2c74469d0
efrice <719431749@qq.com>
2023-12-03 17:55:32 +0800

    refactor: step16 appleSauce rename totalAmount

--- src/test/start.spec.js
+++ src/test/start.spec.js
@@ -16,7 +16,8 @@ import invoices from '../data/invoices.json'
 // import { statement } from '../step12.js'
 // import { statement } from '../step13.js'
 // import { statement } from '../step14.js'
-import { statement } from '../step15.js'
+// import { statement } from '../step15.js'
+import { statement } from '../step16.js'
 
 test('statement', () => {
   const result = statement(invoices[0], plays)
