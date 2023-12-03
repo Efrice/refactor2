012f6152712e978e205b72df917e2944404f9530
efrice <719431749@qq.com>
2023-12-03 23:47:07 +0800

    refactor: step32 class get volumeCredits

--- src/test/start.spec.js
+++ src/test/start.spec.js
@@ -32,7 +32,8 @@ import invoices from '../data/invoices.json'
 // import { statement } from '../step28/statement.js'
 // import { statement } from '../step29/statement.js'
 // import { statement } from '../step30/statement.js'
-import { statement } from '../step31/statement.js'
+// import { statement } from '../step31/statement.js'
+import { statement } from '../step32/statement.js'
 
 test('statement', () => {
   const result = statement(invoices[0], plays)
