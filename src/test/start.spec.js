526aac58be002027bc5a6e59631a4e9deb606964
efrice <719431749@qq.com>
2023-12-03 17:32:32 +0800

    refactor: step8 volumeCreditsFor

--- src/test/start.spec.js
+++ src/test/start.spec.js
@@ -8,7 +8,8 @@ import invoices from '../data/invoices.json'
 // import { statement } from '../step4.js'
 // import { statement } from '../step5.js'
 // import { statement } from '../step6.js'
-import { statement } from '../step7.js'
+// import { statement } from '../step7.js'
+import { statement } from '../step8.js'
 
 test('statement', () => {
   const result = statement(invoices[0], plays)
