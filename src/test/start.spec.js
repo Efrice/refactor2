5eaa1b59d19b4caf20530341b7ca6384187ef864
efrice <719431749@qq.com>
2023-12-03 17:38:14 +0800

    refactor: step10 format

--- src/test/start.spec.js
+++ src/test/start.spec.js
@@ -10,7 +10,8 @@ import invoices from '../data/invoices.json'
 // import { statement } from '../step6.js'
 // import { statement } from '../step7.js'
 // import { statement } from '../step8.js'
-import { statement } from '../step9.js'
+// import { statement } from '../step9.js'
+import { statement } from '../step10.js'
 
 test('statement', () => {
   const result = statement(invoices[0], plays)
