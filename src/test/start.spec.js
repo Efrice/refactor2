5071c112933c21d0bae53a682159b7348e2249d2
efrice <719431749@qq.com>
2023-12-03 23:05:19 +0800

    refactor: step27 remove invoice, plays

--- src/test/start.spec.js
+++ src/test/start.spec.js
@@ -27,7 +27,8 @@ import invoices from '../data/invoices.json'
 // import { statement } from '../step23.js'
 // import { statement } from '../step24.js'
 // import { statement } from '../step25.js'
-import { statement } from '../step26.js'
+// import { statement } from '../step26.js'
+import { statement } from '../step27.js'
 
 test('statement', () => {
   const result = statement(invoices[0], plays)
